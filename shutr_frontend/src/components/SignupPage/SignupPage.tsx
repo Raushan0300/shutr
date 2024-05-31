82dc24234c6619d1747c36c2592315f9a8134be3
Raushan Kumar <raushankumar2909@gmail.com>
2024-06-01 01:03:17 +0530

    chore: Refactor signup form to handle form submission and display server response

--- shutr_frontend/src/components/SignupPage/SignupPage.tsx
+++ shutr_frontend/src/components/SignupPage/SignupPage.tsx
@@ -2,6 +2,7 @@ import './SignupPage.css';
 import logo from '../../assets/logo.svg';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { postData } from '../Config';
 
 const SignupPage = () => {
   const navigate = useNavigate();
@@ -10,6 +11,21 @@ const SignupPage = () => {
   const [lName, setLName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  const [message, setMessage] = useState<string>('');
+
+  const handleSignup = async(e:React.FormEvent)=>{
+    e.preventDefault();
+    const name = fName + ' ' + lName;
+    const response = await postData('signup', {name, email, password}, {});
+    console.log(response);
+    if(!response.err){
+      navigate('/login');
+    } else {
+      setMessage(response.message);
+    }
+  };
+  
   return (
     <div className='signupMainContainer'>
       <div className='signupLogoContainer'>
@@ -18,13 +34,14 @@ const SignupPage = () => {
       </div>
       <div className='signupSignText'>Sign up for an account</div>
       <div className='signupOrText'>Already have an account? <span className='signupOrSpan' onClick={()=>{navigate('/login')}}>Sign in</span></div>
-      <form className='signupForm'>
-        <input type="text" placeholder='First Name' value={fName} className='signupInput signupInputFName' />
-        <input type="text" placeholder='Last Name' value={lName} className='signupInput signupInputLName' />
-        <input type='email' placeholder='Email address' value={email} className='signupInput signupInputEmail' />
-        <input type='password' placeholder='Password' value={password} className='signupInput signupInputPass' />
-        <button className='signupButton'>Sign In</button>
+      <form className='signupForm' onSubmit={handleSignup}>
+        <input type="text" placeholder='First Name' value={fName} onChange={(e)=>{setFName(e.target.value)}} className='signupInput signupInputFName' />
+        <input type="text" placeholder='Last Name' value={lName} onChange={(e)=>{setLName(e.target.value)}} className='signupInput signupInputLName' />
+        <input type='email' placeholder='Email address' value={email} onChange={(e)=>{setEmail(e.target.value)}} className='signupInput signupInputEmail' />
+        <input type='password' placeholder='Password' value={password} onChange={(e)=>{setPassword(e.target.value)}} className='signupInput signupInputPass' />
+        <button type='submit' className='signupButton'>Sign Up</button>
       </form>
+      {message && <div className='signupMessage'>{message}</div>}
     </div>
   )
 };
