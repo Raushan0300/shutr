5f2e77ee7d15641568ec3bfe1e51769d5034aa73
kumar Devashish <kumardevashish000@gmail.com>
2024-05-31 23:46:34 +0530

    chore: update MongoDB connection URI handling

--- shutr_backend/connection.js
+++ shutr_backend/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/shutr';
+const URI = process.env.MONGODB_URI;
 
 mongoose.connect(URI).then(()=>{
     console.log('Connected to MongoDB');
