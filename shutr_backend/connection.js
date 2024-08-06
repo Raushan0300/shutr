ed062e146ae123de9823a5f0cb0086d6801b8abd
Raushan Kumar <raushankumar2909@gmail.com>
2024-08-06 22:42:50 +0530

    chore: Remove unnecessary files and configurations

--- shutr_backend/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = process.env.MONGODB_URI;
-
-mongoose.connect(URI).then(()=>{
-    console.log('Connected to MongoDB');
-}).catch(err => console.log(err));
